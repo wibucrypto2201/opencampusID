fc466f246862df6591255b1cd5c43860ed27c2d4
wibucrypto <56129456+wibucrypto2201@users.noreply.github.com>
2024-07-10 12:21:25 +0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,7 @@
+pip install selenium==4.9.0
+pip install selenium_authenticated_proxy
+pip install fake_useragent
+pip install random
+pip install pycountry
+pip install simplegmail
+pip install random_username
